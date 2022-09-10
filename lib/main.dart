12be784399030eb461bcfacc8e855afa0db3ebb0
Jitesh117 <sahookumarjitesh@gmail.com>
2022-09-10 09:55:16 +0530

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyBIGlq9J1-jSBA-DKk_RXQee9FkUIse9M4',
+      appId: '1:921575750677:web:03f0cb939af9aab9e6a21c',
+      messagingSenderId: '921575750677',
+      projectId: 'instagram-tut-a6274',
+      storageBucket: 'instagram-tut-a6274.appspot.com',
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -20,10 +33,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
       theme: ThemeData.dark(),
-      home: ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      // home: ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home:LoginScreen(),
     );
   }
 }
